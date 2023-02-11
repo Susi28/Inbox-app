23c77cf62afc6ce60cec209e4ac0edfa2cf7568e
Susi28 <sushant281996@gmail.com>
2023-02-12 00:38:35 +0530

    First Commit

---
+++ src/main/java/com/MessagingApplication/Inboxapp/InboxApp.java
@@ -0,0 +1,26 @@
+package com.MessagingApplication.Inboxapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.nio.file.Path;
+
+@SpringBootApplication
+@RestController
+public class InboxApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxApp.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+}
